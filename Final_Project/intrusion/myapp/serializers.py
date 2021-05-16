c2f010757edf01b666027838d0f4bb96553ad12d
aaqeel <anas3qeel@gmail.com>
2021-05-15 23:41:20 -0400

    ..

--- Final_Project/intrusion/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from myapp.models import Room
-from rest_framework import serializers
-
-class RoomSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Room
-        fields = ('url', 'name')
